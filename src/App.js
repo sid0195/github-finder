f447c2a5c09b63d485a9502631d1d595e76d548f
Sidhartha Singh <42sidharthasingh@sony.com>
2019-07-06 21:24:08 -0400

    Functionality complete, before refactors

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ class App extends Component {
     users: [],
     user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   };
 
   async componentDidMount() {
@@ -51,6 +52,18 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // Get users repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
   // Clear users from state
   clearUsers = () => {
     this.setState({ users: [], loading: false });
@@ -64,7 +77,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading, user } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -96,7 +109,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
